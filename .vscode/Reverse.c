53a360807ad6d4895f2b49375083fd495922401f
Yamin <mohammadyamin159263@gmail.com>
2025-09-15 16:55:05 +0600

    String reverse

--- .vscode/Reverse.c
+++ .vscode/Reverse.c
@@ -2,10 +2,10 @@
 #include<string.h>
 int main()
 {
-    char a[67]="reverse";
+    char a[67]="UIU";
     int i,j,temp;
-    int sn=strlen(a);
-    for(i=0,j=sn-1;i<=j;i++,j--){
+    int length=strlen(a);
+    for(i=0,j=length-1;i<=j;i++,j--){
         temp=a[i];
         a[i]=a[j];
         a[j]=temp;
